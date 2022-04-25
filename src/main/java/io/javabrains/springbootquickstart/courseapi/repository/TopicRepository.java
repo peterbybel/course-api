d8380e10266e08ee02c5b57ed8c116f7ca096908
Peter <peterbybel@gmail.com>
2022-04-25 08:51:57 -0500

    initial commit

---
+++ src/main/java/io/javabrains/springbootquickstart/courseapi/repository/TopicRepository.java
@@ -0,0 +1,9 @@
+package io.javabrains.springbootquickstart.courseapi.repository;
+
+import io.javabrains.springbootquickstart.courseapi.model.Topic;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TopicRepository extends CrudRepository<Topic, String> {
+
+
+}
